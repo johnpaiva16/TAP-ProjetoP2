5c575c0ebc757b672fd2c73bc53266a97e905921
jesp <jesp@SUPORTE002323.windows.cin.ufpe.br>
2018-06-11 19:11:42 -0300

    teste commit

--- src/view/TesteMain.java
+++ src/view/TesteMain.java
@@ -1,13 +1,15 @@
 package view;
 
 import controller.ClienteController;
+import dao.ClienteDAO;
 import dao.GenericDAO;
+import java.util.List;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import model.Cliente;
-
+import model.Endereco;
 /**
  *
  * @author jesp
@@ -17,22 +19,27 @@ public class TesteMain extends Application {
     public static void main(String[] args) {
         //launch(args);
         
-        GenericDAO dao = new GenericDAO();
+        //GenericDAO dao = new GenericDAO();
         ClienteController cc = new ClienteController();
         Cliente c = new Cliente();
-
-        c.setCod(2);
+       // ClienteDAO cliDao = new ClienteDAO();
+        
+        Endereco end = new Endereco();
+        //end.setCep("54270110");
+       // c.setCod(1);
         c.setNome("John");
         c.setCpf("09612582416");
-        c.setRg("12124");
-
-       cc.deleteClient(c);
-       // cc.saveClient(c);
-        //cc.updateClient(c);
+        c.setRg("121323224");
+        c.setEndereco(end);
+                     
+        
+      //cc.deleteClient(c);
+       cc.saveClient(c);
+     //   cc.updateClient(c);
         //dao.save(c);
         //dao.update(c);
         //dao.delete(c);
-        /*List<Cliente> l = dao.listAll("cliente");
+       /*List<Cliente> l = cc.listAllClient();
         for (Cliente cl : l) {
             System.out.println(cl.getCod() + " | " + cl.getNome());
 
